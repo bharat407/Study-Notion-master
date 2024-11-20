ba528c25558586c0be367b7290a66ad28baf6ad2
Bharat Kushwah <bharatkushwah407@gmail.com>
2024-11-20 23:37:49 +0530

    Spinner Added

---
+++ src/Components/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="grid justify-items-center w-[100%] min-h-screen">
+      <div class="loader"></div>
+    </div>
+  );
+};
+
+export default Spinner;
